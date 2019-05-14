4cc967fd7c738c80b5da2286527ef7ebcae9f401
thiago <thiagocury@gmail.com>
2019-05-13 21:44:31 -0300

    Calculadora de IMC em Flutter

---
+++ imc/android/app/src/main/java/br/eti/thiagocury/imc/MainActivity.java
@@ -0,0 +1,13 @@
+package br.eti.thiagocury.imc;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
